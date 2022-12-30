26a9ee5527a9252c150051c8e4a1afefa41f1ca7
ANUVISHW <anup.vishwakarma.externe@odigo.com>
2022-12-31 01:34:28 +0530

    added new file and updated existing program

--- 1. frequencyCounter.js
+++ 1. frequencyCounter.js
@@ -1,23 +1,27 @@
-// WAP to check if the 2nd array consist square of all numbers of 1st array.
+// Write a function which accepts two arrays. The function should return true if every value in the array
+// has its corresponding value squared in the second array. The frequency of values must be the same.
 
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   else {
-    const arrElCount = arr => arr.reduce((acc, crr) => {
-      acc[crr] = (acc[crr] || 0) + 1;
-      return acc;
-    }, {});
-
-    const arr1Count = arrElCount(arr1);
-    const arr2Count = arrElCount(arr2);
-
-    for (const key in arr1Count) {
-        const arr2El = arr1Count[key] ** 2;
-        if (arr2Count.hasOwnProperty(arr2El) && arr1Count[key] === arr2Count[arr2El]) continue;
-        else return false;
+    if (arr1.length === 0 || arr2.length === 0) return true;
+    else {
+      const arrElCount = arr => arr.reduce((acc, crr) => {
+        acc[crr] = (acc[crr] || 0) + 1;
+        return acc;
+      }, {});
+  
+      const arr1Count = arrElCount(arr1);
+      const arr2Count = arrElCount(arr2);
+  
+      for (const key in arr1Count) {
+          const arr2El = arr1Count[key] ** 2;
+          if (arr2Count.hasOwnProperty(arr2El) && arr1Count[key] === arr2Count[arr2El]) continue;
+          else return false;
+      }
+  
+      return true;
     }
-
-    return true;
   }
 }
 
---
+++ 2. frequencyCounterAnagram.js
@@ -0,0 +1,28 @@
+// Given two strings, Write a function to determine if the second string is an anagram of the first.
+// An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed by iceman.
+
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  else {
+    if (str1 === '' || str2 === '') return true;
+    else {
+      const subStrCount = str => str.split('').reduce((acc, crr) => {
+        acc[crr] = (acc[crr] || 0) + 1;
+        return acc;
+      }, {});
+  
+      const str1Count = subStrCount(str1);
+      const str2Count = subStrCount(str2);
+  
+      for (const key in str1Count) {
+          if (str2Count.hasOwnProperty(key) && str1Count[key] === str2Count[key]) continue;
+          else return false;
+      }
+  
+      return true;
+    }
+  }
+}
+
+const result = validAnagram('anagram', 'nagaram');
+console.log(result);
